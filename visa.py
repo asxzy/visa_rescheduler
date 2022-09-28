857c6ff5f3b81b56c9ba41eaa15ea37cd3c41c3a
Yi Zhang <asxzy@asxzy.net>
2022-09-27 19:27:25 -0600

    update

--- visa.py
+++ visa.py
@@ -1,9 +1,9 @@
 # -*- coding: utf8 -*-
 
+import smtplib
 import time
 import json
 import random
-import platform
 import configparser
 from datetime import datetime
 
@@ -20,32 +20,38 @@ from sendgrid.helpers.mail import Mail
 
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("config.ini")
 
-USERNAME = config['USVISA']['USERNAME']
-PASSWORD = config['USVISA']['PASSWORD']
-SCHEDULE_ID = config['USVISA']['SCHEDULE_ID']
-MY_SCHEDULE_DATE = config['USVISA']['MY_SCHEDULE_DATE']
-COUNTRY_CODE = config['USVISA']['COUNTRY_CODE'] 
-FACILITY_ID = config['USVISA']['FACILITY_ID']
+USERNAME = config["INFO"]["USERNAME"]
+PASSWORD = config["INFO"]["PASSWORD"]
+SCHEDULE_ID = config["INFO"]["SCHEDULE_ID"]
+MY_SCHEDULE_DATE = config["INFO"]["MY_SCHEDULE_DATE"]
+COUNTRY_CODE = config["INFO"]["COUNTRY_CODE"]
+FACILITY_ID = config["INFO"]["FACILITY_ID"]
 
-SENDGRID_API_KEY = config['SENDGRID']['SENDGRID_API_KEY']
-PUSH_TOKEN = config['PUSHOVER']['PUSH_TOKEN']
-PUSH_USER = config['PUSHOVER']['PUSH_USER']
+SENDGRID_API_KEY = config["SENDGRID"]["SENDGRID_API_KEY"]
+PUSH_TOKEN = config["PUSHOVER"]["PUSH_TOKEN"]
+PUSH_USER = config["PUSHOVER"]["PUSH_USER"]
+EMAIL_HOST = config["EMAIL"]["HOST"]
+EMAIL_PORT = config["EMAIL"]["PORT"]
+EMAIL_USERNAME = config["EMAIL"]["USERNAME"]
+EMAIL_PASSWORD = config["EMAIL"]["PASSWORD"]
 
-LOCAL_USE = config['CHROMEDRIVER'].getboolean('LOCAL_USE')
-HUB_ADDRESS = config['CHROMEDRIVER']['HUB_ADDRESS']
+LOCAL_USE = config["CHROMEDRIVER"].getboolean("LOCAL_USE")
+HUB_ADDRESS = config["CHROMEDRIVER"]["HUB_ADDRESS"]
 
-REGEX_CONTINUE = "//a[contains(text(),'Continuar')]"
+REGEX_CONTINUE = f"//a[contains(text(),'{config['INFO']['CONTINUE']}')]"
 
 
 # def MY_CONDITION(month, day): return int(month) == 11 and int(day) >= 5
-def MY_CONDITION(month, day): return True # No custom condition wanted for the new scheduled date
+def MY_CONDITION(month, day):
+    return True  # No custom condition wanted for the new scheduled date
+
 
 STEP_TIME = 0.5  # time between steps (interactions with forms): 0.5 seconds
-RETRY_TIME = 60*10  # wait time between retries/checks for available dates: 10 minutes
-EXCEPTION_TIME = 60*30  # wait time when an exception occurs: 30 minutes
-COOLDOWN_TIME = 60*60  # wait time when temporary banned (empty list): 60 minutes
+RETRY_TIME = 60 * 10  # wait time between retries/checks for available dates: 10 minutes
+EXCEPTION_TIME = 60 * 30  # wait time when an exception occurs: 30 minutes
+BANNED_COOLDOWN_TIME = 60 * 60  # wait time when temporary banned (empty list): 60 minutes
 
 DATE_URL = f"https://ais.usvisa-info.com/{COUNTRY_CODE}/niv/schedule/{SCHEDULE_ID}/appointment/days/{FACILITY_ID}.json?appointments[expedite]=false"
 TIME_URL = f"https://ais.usvisa-info.com/{COUNTRY_CODE}/niv/schedule/{SCHEDULE_ID}/appointment/times/{FACILITY_ID}.json?date=%s&appointments[expedite]=false"
@@ -57,11 +63,7 @@ def send_notification(msg):
     print(f"Sending notification: {msg}")
 
     if SENDGRID_API_KEY:
-        message = Mail(
-            from_email=USERNAME,
-            to_emails=USERNAME,
-            subject=msg,
-            html_content=msg)
+        message = Mail(from_email=USERNAME, to_emails=USERNAME, subject=msg, html_content=msg)
         try:
             sg = SendGridAPIClient(SENDGRID_API_KEY)
             response = sg.send(message)
@@ -73,12 +75,33 @@ def send_notification(msg):
 
     if PUSH_TOKEN:
         url = "https://api.pushover.net/1/messages.json"
-        data = {
-            "token": PUSH_TOKEN,
-            "user": PUSH_USER,
-            "message": msg
-        }
+        data = {"token": PUSH_TOKEN, "user": PUSH_USER, "message": msg}
         requests.post(url, data)
+
+    if EMAIL_HOST:
+        sent_from = EMAIL_USERNAME
+        to = set([EMAIL_USERNAME])
+        subject = "US Visa Appointment Checker"
+
+        email_text = ""
+        email_text += f"From: {sent_from}"
+        email_text += f"To: {', '.join(to)}"
+        email_text += f"Subject: {subject}"
+        email_text += f"Subject: {subject}"
+        email_text += f"\n{msg}\n"
+
+        try:
+            server = smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT)
+            server.ehlo()
+            server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+            server.sendmail(sent_from, to, email_text)
+            server.close()
+
+            print("Email sent!")
+        except Exception as e:
+            print("Something went wrong while sending the email")
+            print(e)
+            print(e.__traceback__)
 
 
 def get_driver():
@@ -87,6 +110,7 @@ def get_driver():
     else:
         dr = webdriver.Remote(command_executor=HUB_ADDRESS, options=webdriver.ChromeOptions())
     return dr
+
 
 driver = get_driver()
 
@@ -115,27 +139,26 @@ def login():
 
 def do_login_action():
     print("\tinput email")
-    user = driver.find_element(By.ID, 'user_email')
+    user = driver.find_element(By.ID, "user_email")
     user.send_keys(USERNAME)
     time.sleep(random.randint(1, 3))
 
     print("\tinput pwd")
-    pw = driver.find_element(By.ID, 'user_password')
+    pw = driver.find_element(By.ID, "user_password")
     pw.send_keys(PASSWORD)
     time.sleep(random.randint(1, 3))
 
     print("\tclick privacy")
-    box = driver.find_element(By.CLASS_NAME, 'icheckbox')
-    box .click()
+    box = driver.find_element(By.CLASS_NAME, "icheckbox")
+    box.click()
     time.sleep(random.randint(1, 3))
 
     print("\tcommit")
-    btn = driver.find_element(By.NAME, 'commit')
+    btn = driver.find_element(By.NAME, "commit")
     btn.click()
     time.sleep(random.randint(1, 3))
 
-    Wait(driver, 60).until(
-        EC.presence_of_element_located((By.XPATH, REGEX_CONTINUE)))
+    Wait(driver, 60).until(EC.presence_of_element_located((By.XPATH, REGEX_CONTINUE)))
     print("\tlogin successful!")
 
 
@@ -145,7 +168,7 @@ def get_date():
         login()
         return get_date()
     else:
-        content = driver.find_element(By.TAG_NAME, 'pre').text
+        content = driver.find_element(By.TAG_NAME, "pre").text
         date = json.loads(content)
         return date
 
@@ -153,7 +176,7 @@ def get_date():
 def get_time(date):
     time_url = TIME_URL % date
     driver.get(time_url)
-    content = driver.find_element(By.TAG_NAME, 'pre').text
+    content = driver.find_element(By.TAG_NAME, "pre").text
     data = json.loads(content)
     time = data.get("available_times")[-1]
     print(f"Got time successfully! {date} {time}")
@@ -168,34 +191,38 @@ def reschedule(date):
     driver.get(APPOINTMENT_URL)
 
     data = {
-        "utf8": driver.find_element(by=By.NAME, value='utf8').get_attribute('value'),
-        "authenticity_token": driver.find_element(by=By.NAME, value='authenticity_token').get_attribute('value'),
-        "confirmed_limit_message": driver.find_element(by=By.NAME, value='confirmed_limit_message').get_attribute('value'),
-        "use_consulate_appointment_capacity": driver.find_element(by=By.NAME, value='use_consulate_appointment_capacity').get_attribute('value'),
-        "appointments[consulate_appointment][facility_id]": FACILITY_ID,
-        "appointments[consulate_appointment][date]": date,
-        "appointments[consulate_appointment][time]": time,
+        "utf8": driver.find_element(by=By.NAME, value="utf8").get_attribute("value"),
+        "authenticity_token": driver.find_element(by=By.NAME, value="authenticity_token").get_attribute("value"),
+        "confirmed_limit_message": driver.find_element(by=By.NAME, value="confirmed_limit_message").get_attribute("value"),
+        "use_consulate_appointment_capacity": driver.find_element(
+            by=By.NAME, value="use_consulate_appointment_capacity"
+        ).get_attribute("value"),
+        "appointments[asc_appointment][facility_id]": FACILITY_ID,
+        "appointments[asc_appointment][date]": date,
+        "appointments[asc_appointment][time]": time,
     }
 
     headers = {
         "User-Agent": driver.execute_script("return navigator.userAgent;"),
         "Referer": APPOINTMENT_URL,
-        "Cookie": "_yatri_session=" + driver.get_cookie("_yatri_session")["value"]
+        "Cookie": "_yatri_session=" + driver.get_cookie("_yatri_session")["value"],
     }
 
     r = requests.post(APPOINTMENT_URL, headers=headers, data=data)
-    if(r.text.find('Successfully Scheduled') != -1):
+    if r.text.find("Successfully Scheduled") != -1:
+        global MY_SCHEDULE_DATE
+        MY_SCHEDULE_DATE = date
         msg = f"Rescheduled Successfully! {date} {time}"
         send_notification(msg)
-        EXIT = True
     else:
         msg = f"Reschedule Failed. {date} {time}"
+        print(msg)
         send_notification(msg)
 
 
 def is_logged_in():
     content = driver.page_source
-    if(content.find("error") != -1):
+    if content.find("error") != -1:
         return False
     return True
 
@@ -203,7 +230,7 @@ def is_logged_in():
 def print_dates(dates):
     print("Available dates:")
     for d in dates:
-        print("%s \t business_day: %s" % (d.get('date'), d.get('business_day')))
+        print("%s \t business_day: %s" % (d.get("date"), d.get("business_day")))
     print()
 
 
@@ -217,66 +244,51 @@ def get_available_date(dates):
         my_date = datetime.strptime(MY_SCHEDULE_DATE, "%Y-%m-%d")
         new_date = datetime.strptime(date, "%Y-%m-%d")
         result = my_date > new_date
-        print(f'Is {my_date} > {new_date}:\t{result}')
+        print(f"Is {my_date} > {new_date}:\t{result}")
         return result
 
     print("Checking for an earlier date:")
     for d in dates:
-        date = d.get('date')
+        date = d.get("date")
         if is_earlier(date) and date != last_seen:
-            _, month, day = date.split('-')
-            if(MY_CONDITION(month, day)):
+            _, month, day = date.split("-")
+            if MY_CONDITION(month, day):
                 last_seen = date
                 return date
-
-
-def push_notification(dates):
-    msg = "date: "
-    for d in dates:
-        msg = msg + d.get('date') + '; '
-    send_notification(msg)
 
 
 if __name__ == "__main__":
     login()
     retry_count = 0
-    while 1:
-        if retry_count > 6:
-            break
+    while True:
+        retry_count += 1
         try:
+            print(f"attempt: {retry_count}")
+            print(f"current schedule: {MY_SCHEDULE_DATE}")
             print("------------------")
-            print(datetime.today())
-            print(f"Retry count: {retry_count}")
-            print()
 
-            dates = get_date()[:5]
+            dates = get_date()
             if not dates:
-              msg = "List is empty"
-              send_notification(msg)
-              EXIT = True
+                print(f"List is empty, possibility due to temporary ban. Sleep {BANNED_COOLDOWN_TIME}s before retrying")
+                time.sleep(BANNED_COOLDOWN_TIME)
+                continue
+
             print_dates(dates)
             date = get_available_date(dates)
-            print()
-            print(f"New date: {date}")
             if date:
+                print(f"New date: {date}")
                 reschedule(date)
-                push_notification(dates)
+            else:
+                print(f"No better date avaliable, currently scheduled for {MY_SCHEDULE_DATE}.")
+                time.sleep(RETRY_TIME)
 
-            if(EXIT):
+            if EXIT:
                 print("------------------exit")
                 break
 
-            if not dates:
-              msg = "List is empty"
-              send_notification(msg)
-              #EXIT = True
-              time.sleep(COOLDOWN_TIME)
-            else:
-              time.sleep(RETRY_TIME)
-
-        except:
-            retry_count += 1
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(f"Failed to pull the dates from web. Retrying in {EXCEPTION_TIME}s")
+            print(e)
             time.sleep(EXCEPTION_TIME)
-
-    if(not EXIT):
-        send_notification("HELP! Crashed.")
